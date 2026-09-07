3008fd7f6d8c66669eda7545b32b6236b379c8c7
MeisterBOBtr <berndtj440@gmail.com>
2026-09-07 23:24:34 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://pdbvqsuyjblijvubwiph.supabase.co";
+const SUPABASE_KEY = "sb_publishable_8DCc8cg3WHp0uR2KcnHCQw_CdhxrkpQ";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
