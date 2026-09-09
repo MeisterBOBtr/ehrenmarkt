9c8af616b79b752dcae33eb0ea23328c6f4868dc
MeisterBOBtr <berndtj440@gmail.com>
2026-09-09 02:53:12 +0200

    Update materialauftrag.js

--- JS/materialauftrag.js
+++ JS/materialauftrag.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", async () => {
 
-    const supabase = window.supabaseClient;
-
+    const supabase = supabaseClient;
     // ============================================================
     // FORMULARELEMENTE
     // ============================================================
