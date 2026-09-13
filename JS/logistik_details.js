5d2d7ea473cc676a27f8b74b6aa7691351db5002
MeisterBOBtr <berndtj440@gmail.com>
2026-09-13 16:09:35 +0200

    Update logistik_details.js

--- JS/logistik_details.js
+++ JS/logistik_details.js
@@ -915,6 +915,13 @@ async function init() {
         showError(
             "Die Logistik-Detailseite konnte nicht geladen werden."
         );
+
+            } finally {
+        const loadingSection = document.getElementById("loadingSection");
+
+        if (loadingSection) {
+            loadingSection.style.display = "none";
+        }
     }
 }
 
